e892049c9f205b58136a21e72feedc94faeb8f98
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-11-03 20:34:33 +0100

    Implementacija klase FiksniBroj

--- src/main/java/ba/unsa/etf/rpr/FiksniBroj.java
+++ src/main/java/ba/unsa/etf/rpr/FiksniBroj.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Objects;
+
 public class FiksniBroj extends TelefonskiBroj{
 
     private Grad grad;
@@ -7,17 +9,26 @@ public class FiksniBroj extends TelefonskiBroj{
     private String broj;
 
     public FiksniBroj(Grad grad, String broj){
+        if(grad == null || broj == null || broj.length() != 7)
+            throw new IllegalArgumentException("Argumenti nisu ispravni!");
         this.grad = grad;
         this.broj = broj;
     }
 
+    public Grad getGrad(){
+        return grad;
+    }
+
+    public String getBroj(){
+        return broj;
+    }
     @Override
     public String ispisi() {
-        return null;
+        return grad.getPozivniBroj() + '/' + broj;
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return Objects.hash(grad, broj);
     }
 }
