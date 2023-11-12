8d662c1085ddfc5bf7ec281de037de636af6c5c1
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-11-12 08:20:10 +0100

    Dodavanje klase LaptopDaoSerializableFile koja implementira interfejs LaptopDao

---
+++ lv4-z1/src/main/java/ba/unsa/etf/rpr/LaptopDaoSerializableFile.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class LaptopDaoSerializableFile implements LaptopDao{
+
+    private File file;
+    private ArrayList<Laptop> laptopi;
+
+
+    @Override
+    public void dodajLaptopUListu(Laptop laptop) {
+
+    }
+
+    @Override
+    public void dodajLaptopUFile(Laptop laptop) {
+
+    }
+
+    @Override
+    public Laptop getLaptop(String procesor) {
+        return null;
+    }
+
+    @Override
+    public void napuniListu(ArrayList<Laptop> laptopi) {
+
+    }
+
+    @Override
+    public ArrayList<Laptop> vratiPodatkeIzDatoteke() {
+        return null;
+    }
+}
