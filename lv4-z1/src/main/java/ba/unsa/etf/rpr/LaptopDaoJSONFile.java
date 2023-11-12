1da122709b653cc808bb1cfa95b41708ba9b7aa7
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-11-12 10:05:03 +0100

    Dodavanje i implementacija LaptopDaoJSONFile

---
+++ lv4-z1/src/main/java/ba/unsa/etf/rpr/LaptopDaoJSONFile.java
@@ -0,0 +1,39 @@
+package ba.unsa.etf.rpr;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class LaptopDaoJSONFile implements LaptopDao{
+    private File file;
+    private ArrayList<Laptop> laptopi;
+
+    public LaptopDaoJSONFile(){
+        file = new File("/json_file.json");
+        laptopi = new ArrayList<>();
+    }
+
+    @Override
+    public void dodajLaptopUListu(Laptop laptop) {
+
+    }
+
+    @Override
+    public void dodajLaptopUFile(Laptop laptop) {
+
+    }
+
+    @Override
+    public Laptop getLaptop(String procesor) {
+        return null;
+    }
+
+    @Override
+    public void napuniListu(ArrayList<Laptop> laptopi) {
+
+    }
+
+    @Override
+    public ArrayList<Laptop> vratiPodatkeIzDatoteke() {
+        return null;
+    }
+}
