df68d173ecd13bb43d22c8380b295930d2b2f37b
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-11-18 16:57:54 +0100

    implementacija konstruktora i metode setOcjena

--- src/main/java/ba/unsa/etf/rpr/Ocjena.java
+++ src/main/java/ba/unsa/etf/rpr/Ocjena.java
@@ -3,4 +3,14 @@ package ba.unsa.etf.rpr;
 public class Ocjena {
     private LicneInformacije osoba;
     private int ocjena;
+
+    public Ocjena(LicneInformacije osoba, int ocjena){
+        this.osoba = osoba;
+        this.ocjena = ocjena;
+    }
+
+    public void setOcjena(int x){
+        if(x < 0 || x > 10) throw new IllegalArgumentException("Ocjena izvan dozvoljenog opsega!");
+        this.ocjena = x;
+    }
 }
