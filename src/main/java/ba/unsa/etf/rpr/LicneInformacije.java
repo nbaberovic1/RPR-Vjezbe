2b433f2a2cb8cfc5c05785240365afa17cf6443a
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-11-18 09:09:26 +0100

    Dodavanje i implementacija klase LicneInformacije

---
+++ src/main/java/ba/unsa/etf/rpr/LicneInformacije.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr;
+
+public class LicneInformacije {
+    private String ime;
+    private String prezime;
+
+    public String getIme(){
+        return this.ime;
+    }
+
+    public void setIme(String ime){
+        this.ime = ime;
+    }
+
+    public String getPrezime(){
+        return this.prezime;
+    }
+
+    public void setPrezime(String prezime){
+        this.prezime = prezime;
+    }
+}
