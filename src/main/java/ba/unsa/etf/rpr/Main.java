67d3eef2e8b6fd545b8a1d3fe0bda9f63916f08c
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-11-18 21:43:25 +0100

    implementacija unosa informacija o osobi

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -3,11 +3,61 @@ package ba.unsa.etf.rpr;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner ulaz = new Scanner(System.in);
-        System.out.println("""
-      Ako ste Student pritisnite 1,
-      ako ste nastavnik pritisnike 2,
-      ako ste neko drugi pritisnite 3: """);
+
+    public static void main (String[]args){
+        while(true){
+            Scanner ulaz = new Scanner(System.in);
+            System.out.println("""
+                    Ako ste student pritisnite 1,
+                    ako ste nastavnik pritisnike 2,
+                    ako ste neko drugi pritisnite 3: """);
+
+            int uloga = ulaz.nextInt();
+            if(uloga<1 || uloga>3) {
+                System.out.println("Neispravan unos!");
+                continue;
+            }
+
+            LicneInformacije osoba = null;
+
+            System.out.println("Unesite ime: ");
+            String ime = ulaz.nextLine();
+
+            System.out.println("Unesite prezime: "):
+            String prezime = ulaz.nextLine();
+
+            switch (uloga) {
+                case 1:
+                    osoba = new InformacijeOStudentu();
+                    System.out.println("Unesite godinu studija: ");
+                    String godinaStudija = ulaz.nextLine();
+                    System.out.println("Unesite broj indexa: ");
+                    String brojIndexa = ulaz.nextLine();
+                    osoba.setIme(ime);
+                    osoba.setPrezime(prezime);
+                    InformacijeOStudentu osobaStudent = (InformacijeOStudentu) osoba;
+                    osobaStudent.setGodinaStudija(godinaStudija);
+                    osobaStudent.setBrojIndexa(brojIndexa);
+                    break;
+                case 2:
+                    osoba = new InformacijeONastavniku();
+                    System.out.println("Unesite titulu: ");
+                    String titula = ulaz.nextLine();
+                    InformacijeONastavniku osobaNastavnik = (InformacijeONastavniku) osoba;
+                    osobaNastavnik.setTitula(titula);
+                    break;
+                case 3:
+                    osoba = new LicneInformacije();
+                    osoba.setIme(ime);
+                    osoba.setPrezime(prezime);
+                    break;
+            }
+
+        }
+
     }
+
+
+
+
 }
