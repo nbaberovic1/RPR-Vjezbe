730461f12f5db88a0b80dde6218944b66e494d79
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-11-18 09:19:24 +0100

    Dodavanje i implementacija klase InformacijeONastavniku

---
+++ src/main/java/ba/unsa/etf/rpr/InformacijeONastavniku.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+public class InformacijeONastavniku extends LicneInformacije{
+    private String titula;
+
+    public String getTitula(){
+        return this.titula;
+    }
+
+    public void setTitula(String titula){
+        this.titula = titula;
+    }
+}
