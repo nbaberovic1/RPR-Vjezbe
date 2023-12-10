4f909a6cac99efec605e2dd43e2b2f5797a168af
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-12-10 20:43:12 +0100

    implementacija metode initialize i prepravka start metode u main-u

--- src/main/java/ba/unsa/etf/rpr/rprlab78/Controller.java
+++ src/main/java/ba/unsa/etf/rpr/rprlab78/Controller.java
@@ -2,10 +2,55 @@ package ba.unsa.etf.rpr.rprlab78;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class Controller {
+
+    private KorisniciModel model;
+    public TextField fldIme;
+    public TextField fldPrezime;
+    public TextField fldEmail;
+    public TextField fldKorisnickoIme;
+    public TextField fldLozinka;
+    public ListView<Korisnik> listKorisnici;
+
+    public Controller (KorisniciModel m) {
+        model = m;
+    }
     @FXML
     public void initialize() {
+        //fldIme.textProperty().bindBidirectional(model.getTrenutniKorisnik().imeProperty());
+        //fldPrezime.textProperty().bindBidirectional(model.getTrenutniKorisnik().prezimeProperty());
+        //fldEmail.textProperty().bindBidirectional(model.getTrenutniKorisnik().emailProperty());
+        //fldKorisnickoIme.textProperty().bindBidirectional(model.getTrenutniKorisnik().korisnickoImeProperty());
+        //fldLozinka.textProperty().bindBidirectional(model.getTrenutniKorisnik().lozinkaProperty());
+
+        listKorisnici.setItems(model.getKorisnici());
+
+        model.trenutniKorisnikProperty().addListener((obs, oldKorisnik, newKorisnik) -> {
+            if(oldKorisnik != null){
+                fldIme.textProperty().unbindBidirectional(oldKorisnik.imeProperty());
+                fldPrezime.textProperty().unbindBidirectional(oldKorisnik.prezimeProperty());
+                fldEmail.textProperty().unbindBidirectional(oldKorisnik.emailProperty());
+                fldKorisnickoIme.textProperty().unbindBidirectional(oldKorisnik.korisnickoImeProperty());
+                fldLozinka.textProperty().unbindBidirectional(oldKorisnik.lozinkaProperty());
+            }
+
+            if( newKorisnik == null){
+                fldIme.setText("");
+                fldPrezime.setText("");
+                fldEmail.setText("");
+                fldKorisnickoIme.setText("");
+                fldLozinka.setText("");
+            } else {
+                fldIme.textProperty().bindBidirectional(newKorisnik.imeProperty());
+                fldPrezime.textProperty().bindBidirectional(newKorisnik.prezimeProperty());
+                fldEmail.textProperty().bindBidirectional(newKorisnik.emailProperty());
+                fldKorisnickoIme.textProperty().bindBidirectional(newKorisnik.korisnickoImeProperty());
+                fldLozinka.textProperty().bindBidirectional(newKorisnik.lozinkaProperty());
+            }
+        });
     }
 
 }
--- src/main/java/ba/unsa/etf/rpr/rprlab78/Main.java
+++ src/main/java/ba/unsa/etf/rpr/rprlab78/Main.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.rprlab78;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,11 +11,14 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("korisnici.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        KorisniciModel model = new KorisniciModel();
+        model.napuni();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("korisnici.fxml"));
+        loader.setController(new Controller(model));
+        Parent root = loader.load();
         stage.setTitle("Korisnici");
         stage.setResizable(false);
-        stage.setScene(scene);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
