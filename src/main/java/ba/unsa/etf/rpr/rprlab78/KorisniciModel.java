3ada1f386f6d0a905a13d411c606a8ae10cf527b
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-12-10 16:27:20 +0100

    implementacija klase KorisniciModel - geteri, seteri i napuni metoda

---
+++ src/main/java/ba/unsa/etf/rpr/rprlab78/KorisniciModel.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.rprlab78;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class KorisniciModel {
+
+    private ObservableList<Korisnik> korisnici = FXCollections.observableArrayList();
+    private ObjectProperty<Korisnik> trenutniKorisnik = new SimpleObjectProperty<>();
+
+    public Korisnik getTrenutniKorisnik() {
+        return this.trenutniKorisnik.get();
+    }
+
+    public ObjectProperty<Korisnik> trenutniKorisnikProperty() {
+        return this.trenutniKorisnik;
+    }
+
+    public void setTrenutniKorisnik(Korisnik korisnik) {
+        this.trenutniKorisnik.set(korisnik);
+    }
+
+    public ObservableList<Korisnik> getKorisnici() {
+        return this.korisnici;
+    }
+
+    public void napuni() {
+        korisnici.add(new Korisnik("Korisnik", "1", "korisnik1@live.com", "korisnik1", "kor1"));
+        korisnici.add(new Korisnik("Korisnik", "2", "korisnik2@live.com", "korisnik2", "kor2"));
+    }
+}
