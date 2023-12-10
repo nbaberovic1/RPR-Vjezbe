d968b53cba7e14037138310f1527abe7624eb587
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-12-10 16:55:30 +0100

    prepravka controller-a i main klase

---
+++ src/main/java/ba/unsa/etf/rpr/rprlab78/Controller.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.rprlab78;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    public void initialize() {
+    }
+
+}
--- src/main/java/ba/unsa/etf/rpr/rprlab78/Main.java
+++ src/main/java/ba/unsa/etf/rpr/rprlab78/Main.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("korisnici.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("korisnici.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Korisnici");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
