e5824f5970d1b284b001a676875fb725efa5c99b
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2023-12-30 21:09:03 +0100

    implementacija metode dodajGrad

--- src/main/java/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/main/java/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -13,6 +13,9 @@ public class GeografijaDAO {
     private PreparedStatement stmtSviGradovi;
     private PreparedStatement stmtGlavniGrad;
     private PreparedStatement stmtObrisiDrzavu;
+    private PreparedStatement stmtDrzava;
+    private PreparedStatement stmtDodajGrad;
+    private PreparedStatement stmtAzurirajDrzavu;
 
     private void regenerisiBazu () {
         Scanner ulaz = null;
@@ -41,15 +44,21 @@ public class GeografijaDAO {
     private GeografijaDAO() throws SQLException {
         conn = DriverManager.getConnection("jdbc:sqlite:src/main/resources/baza.db");
         try {
-            stmtSviGradovi = conn.prepareStatement("SELECT * FROM grad g, drzava d WHERE g.drzava = d.id ORDER BY g.broj_stanovnika DESC;");
+            stmtSviGradovi = conn.prepareStatement("SELECT * FROM grad g LEFT OUTER JOIN drzava d ON g.drzava = d.id ORDER BY g.broj_stanovnika DESC;");
             stmtGlavniGrad = conn.prepareStatement("SELECT * FROM  drzava d, grad g WHERE d.glavni_grad = g.id and d.naziv = ?;");
             stmtObrisiDrzavu = conn.prepareStatement("DELETE FROM drzava WHERE naziv = ?;");
+            stmtDrzava = conn.prepareStatement("SELECT * FROM drzava WHERE naziv = ?;");
+            stmtDodajGrad = conn.prepareStatement("INSERT INTO grad VALUES (?, ?, ?, ?);");
+            stmtAzurirajDrzavu = conn.prepareStatement("UPDATE drzava SET glavni_grad = ? WHERE id = ?;");
         } catch ( SQLException e ) {
             regenerisiBazu();
             try {
-                stmtSviGradovi = conn.prepareStatement("SELECT * FROM grad g, drzava d WHERE g.drzava = d.id ORDER BY g.broj_stanovnika DESC;");
+                stmtSviGradovi = conn.prepareStatement("SELECT * FROM grad g LEFT OUTER JOIN drzava d ON g.drzava = d.id ORDER BY g.broj_stanovnika DESC;");
                 stmtGlavniGrad = conn.prepareStatement("SELECT * FROM  drzava d, grad g WHERE d.glavni_grad = g.id and d.naziv = ?;");
                 stmtObrisiDrzavu = conn.prepareStatement("DELETE FROM drzava WHERE naziv = ?;");
+                stmtDrzava = conn.prepareStatement("SELECT * FROM drzava WHERE naziv = ?;");
+                stmtDodajGrad = conn.prepareStatement("INSERT INTO grad VALUES (?, ?, ?, ?);");
+                stmtAzurirajDrzavu = conn.prepareStatement("UPDATE drzava SET glavni_grad = ? WHERE id = ?;");
             } catch ( SQLException e1) {
                 e1.printStackTrace();
             }
@@ -102,4 +111,51 @@ public class GeografijaDAO {
             e.printStackTrace();
         }
     }
+
+    public void dodajGrad(Grad grad) {
+        try {
+            stmtDodajGrad.setInt(1, grad.getId());
+            stmtDodajGrad.setString(2, grad.getNaziv());
+            stmtDodajGrad.setInt(3, grad.getBrojStanovnika());
+            if (grad.getDrzava() != null) {
+                stmtDrzava.setString(1, grad.getDrzava().getNaziv());
+                ResultSet rezultatDrzava = stmtDrzava.executeQuery();
+                if(rezultatDrzava.next()) {
+                    stmtDodajGrad.setInt(4, rezultatDrzava.getInt(1));
+                    stmtDodajGrad.executeUpdate();
+                    if(grad.getId() == grad.getDrzava().getGlavniGrad()) {
+                        stmtAzurirajDrzavu.setInt(1, grad.getId());
+                        stmtAzurirajDrzavu.setInt(2, rezultatDrzava.getInt(1));
+                        stmtAzurirajDrzavu.executeUpdate();
+                    }
+                } else {
+                    System.out.println("Nema drzave u kojoj se nalazi navedeni grad!");
+                }
+            } else {
+                stmtDodajGrad.setNull(4, Types.INTEGER);
+                stmtDodajGrad.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -10,16 +10,21 @@ public class Main {
         for(Grad g : gradovi) {
             System.out.println(g.getNaziv());
         }
+/*
         Grad g = geo.glavniGrad("Austrija");
         Drzava d = g.getDrzava();
         System.out.println(System.lineSeparator() + g.getNaziv());
         System.out.println(d.getNaziv() + System.lineSeparator());
 
-        geo.obrisiDrzavu("Velika Britanija");
+        geo.obrisiDrzavu("Velika Britanija");*/
+
+        //geo.dodajGrad(new Grad(7, "Hamburg", 1786448, null));
+
+        geo.dodajGrad(new Grad(6, "Nice", 348721, new Drzava(1, "Francuska", 1)));
 
         gradovi = geo.gradovi();
-        for(Grad grad : gradovi) {
-            System.out.println(grad.getNaziv());
+        for(Grad g1 : gradovi) {
+            System.out.println(g1.getNaziv());
         }
     }
 }
