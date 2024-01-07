1e354e7c551b0f830bb5d8b291d949165dc618d1
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2024-01-07 12:20:59 +0100

    Prepravka klase Grad tako da zadovoljava JavaFXBeans specifikaciju

--- src/main/java/ba/unsa/etf/rpr/lv10_11/Grad.java
+++ src/main/java/ba/unsa/etf/rpr/lv10_11/Grad.java
@@ -1,54 +1,75 @@
 package ba.unsa.etf.rpr.lv10_11;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Grad {
-    private int id;
-    private String naziv;
-    private int brojStanovnika;
-    private Drzava drzava;
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty naziv;
+    private SimpleIntegerProperty brojStanovnika;
+    private ObjectProperty<Drzava> drzava;
 
     public Grad () {
-        this.id = 0;
-        this.naziv = "";
-        this.brojStanovnika = 0;
+        this.id = new SimpleIntegerProperty(0);
+        this.naziv = new SimpleStringProperty("");
+        this.brojStanovnika = new SimpleIntegerProperty(0);
         this.drzava = null;
     }
 
-    public Grad (int id, String naziv, int brojStanovnika, Drzava drzava) {
+    public Grad (SimpleIntegerProperty id, SimpleStringProperty naziv, SimpleIntegerProperty brojStanovnika, ObjectProperty<Drzava> drzava) {
         this.id = id;
         this.naziv = naziv;
         this.drzava = drzava;
         this.brojStanovnika = brojStanovnika;
     }
 
-    public int getId() {
+    public final int getId() {
+        return id.get();
+    }
+
+
+    public final SimpleIntegerProperty idProperty() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
+    public void setId(final int id) {
+        this.id.set(id);
     }
 
-    public String getNaziv() {
+    public final String getNaziv() {
+        return naziv.get();
+    }
+
+    public final SimpleStringProperty nazivProperty() {
         return naziv;
     }
 
-    public void setNaziv(String naziv) {
-        this.naziv = naziv;
+    public void setNaziv(final String naziv) {
+        this.naziv.set(naziv);
     }
 
-    public int getBrojStanovnika() {
+    public final int getBrojStanovnika() {
+        return brojStanovnika.get();
+    }
+
+    public final SimpleIntegerProperty brojStanovnikaProperty() {
         return brojStanovnika;
     }
 
-    public void setBrojStanovnika(int brojStanovnika) {
-        this.brojStanovnika = brojStanovnika;
+    public void setBrojStanovnika(final int brojStanovnika) {
+        this.brojStanovnika.set(brojStanovnika);
     }
 
-    public Drzava getDrzava() {
+    public final Drzava getDrzava() {
+        return drzava.get();
+    }
+
+    public final ObjectProperty<Drzava> drzavaProperty() {
         return drzava;
     }
 
-    public void setDrzava(Drzava drzava) {
-        this.drzava = drzava;
+    public void setDrzava(final Drzava drzava) {
+        this.drzava.set(drzava);
     }
 }
