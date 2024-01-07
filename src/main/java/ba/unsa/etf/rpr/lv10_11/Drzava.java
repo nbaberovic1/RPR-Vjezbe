a5e1397de59a75f99c806c7508f28fdff516a241
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2024-01-07 12:38:49 +0100

    Prepravka klase Drzava tako da zadovoljava JavaFXBeans specifikaciju

--- src/main/java/ba/unsa/etf/rpr/lv10_11/Drzava.java
+++ src/main/java/ba/unsa/etf/rpr/lv10_11/Drzava.java
@@ -1,43 +1,57 @@
 package ba.unsa.etf.rpr.lv10_11;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Drzava {
-    private int id;
-    private String naziv;
-    private int glavniGrad;
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty naziv;
+    private SimpleIntegerProperty glavniGrad;
 
     public Drzava() {
-        this.id = 0;
-        this.naziv = "";
-        this.glavniGrad = 0;
+        this.id = new SimpleIntegerProperty(0);
+        this.naziv = new SimpleStringProperty("");
+        this.glavniGrad = new SimpleIntegerProperty(0);
     }
 
-    public Drzava(int id, String naziv, int glavniGrad) {
+    public Drzava(SimpleIntegerProperty id, SimpleStringProperty naziv, SimpleIntegerProperty glavniGrad) {
         this.id = id;
         this.naziv = naziv;
         this.glavniGrad = glavniGrad;
     }
 
-    public int getId() {
+    public final int getId() {
+        return id.get();
+    }
+
+    public final SimpleIntegerProperty idProperty() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(final int id) {
+        this.id.set(id);
     }
 
-    public String getNaziv() {
+    public final String getNaziv() {
+        return naziv.get();
+    }
+
+    public final SimpleStringProperty nazivProperty() {
         return naziv;
     }
 
-    public void setNaziv(String naziv) {
-        this.naziv = naziv;
+    public void setNaziv(final String naziv) {
+        this.naziv.set(naziv);
     }
 
-    public int getGlavniGrad() {
+    public final int getGlavniGrad() {
+        return glavniGrad.get();
+    }
+
+    public final SimpleIntegerProperty glavniGradProperty() {
         return glavniGrad;
     }
-
-    public void setGlavniGrad(int glavniGrad) {
-        this.glavniGrad = glavniGrad;
+    public void setGlavniGrad(final int glavniGrad) {
+        this.glavniGrad.set(glavniGrad);
     }
 }
